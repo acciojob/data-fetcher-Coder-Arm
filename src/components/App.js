6fbe2113472926b1c5b4c69c22b2c93e30ebd25c
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-09-05 10:58:41 +0530

    push

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import DataDisplay from "./DataDisplay";
 
 const App = () => {
   return (
     <div>
+      <DataDisplay/>
         {/* Do not remove the main div */}
     </div>
   )
---
+++ src/components/DataDisplay.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+const DataDisplay = () => {
+  const [data,setData] = useState("");
+  const [gotData,setGotData] = useState(false)
+useEffect(()=> {
+axios.get("https://dummyjson.com/products")
+.then(response => {
+    setData(response.data)
+    setGotData(true)
+})
+.catch(error => console.log(error))
+})
+    return (
+    <div>
+        { gotData ?<div>
+        <h1>Data Fetched from API</h1>
+      <pre>
+      {JSON.stringify(data)}
+      </pre>
+     </div>
+        :  <div>Loading...</div>}
+    </div>
+  )
+}
+
+export default DataDisplay
